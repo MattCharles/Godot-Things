2e97b658d0e9e7fd83ae66c87db951bdcddb9695
Matt Miller <matthew.c.anthony.miller@gmail.com>
2023-01-26 13:53:18 -0800

    Syncing multiplayer variables

--- Networking/Explained/Multiplayer/Levels/Level1.gd
+++ Networking/Explained/Multiplayer/Levels/Level1.gd
@@ -6,16 +6,18 @@ var host_id
 func _ready():
 	host_id = multiplayer.get_unique_id()
 	host_player = create_player(host_id)
-	var peers = get_tree().get_peers()
+	var peers = multiplayer.get_peers()
 	for peer in peers:
+		print("peer found: " + peer)
 		create_player(peer)
 
 func create_player(id):
+	print("spawning: " + id)
 	var player = preload("res://Characters/Aliens/Player.tscn").instantiate()
 	player.name = str(id)
 	#player.set_network_master(id)
 	#Player positions are randomized different for each player, but in this setup it doesn't matter
 	#If you are going to actually use randomization in a multiplayer game, consider synchronizing rng seeds
-	player.position = Vector2(randf_range(0,1024),randf_range(0,600))
+	player.position = Vector2(randf_range(0,100),randf_range(0,100))
 	add_child(player)
 	return player
