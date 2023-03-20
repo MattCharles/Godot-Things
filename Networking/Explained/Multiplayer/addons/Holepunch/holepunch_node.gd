03a815719baee123782775a16fcdbf4a44e07e9c
Matt Miller <matthew.c.anthony.miller@gmail.com>
2023-03-20 16:05:28 -0700

    Changed holepunch node to handle local testing more gracefully

--- Networking/Explained/Multiplayer/addons/Holepunch/holepunch_node.gd
+++ Networking/Explained/Multiplayer/addons/Holepunch/holepunch_node.gd
@@ -194,7 +194,9 @@ func _ping_peer():
 		if stage < 1: all_info = false
 		if stage < 2: all_confirm = false
 		if stage == 0: #received no contact, send greet
-			if ping_cycles >= response_window:
+			# When running tests on localhost, sometimes the host pings itself forever looking for the client.
+			# Use randomness to give it an out
+			if ping_cycles >= response_window or (randi() % 5 == 0 and peer.port == own_port):
 				_cascade_peer(peer.address,peer.port)
 			else:
 				print("> send greet!")
