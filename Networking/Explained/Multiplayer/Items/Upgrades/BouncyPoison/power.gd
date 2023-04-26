5ec95f680e3542d14bbd816f4fb11771fb8918ab
Matt Miller <matthew.c.anthony.miller@gmail.com>
2023-04-26 12:02:41 -0400

    Bouncy poison card

---
+++ Networking/Explained/Multiplayer/Items/Upgrades/BouncyPoison/power.gd
@@ -0,0 +1,16 @@
+
+extends Node
+
+const card_id := 30
+
+const modifiers := {
+	"bullet_bounces": {
+		"add": 2
+	},
+	"poison_damage": {
+		"add": 8
+	},
+	"poison_duration": {
+		"add": 3.99
+	}
+}
